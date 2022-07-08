ddfc67a1079f816500716103dbe0b17c39e2d4be
kivoie <mindarc14@gmail.com>
2022-07-07 21:34:38 -0400

    import re, and edited message body HTML styling

--- sendemail.py
+++ sendemail.py
@@ -1,4 +1,4 @@
-import smtplib, codecs, os, subprocess, sys, time
+import smtplib, codecs, os, subprocess, sys, time, re
 from datetime import datetime
 
 SMTP_SERVER = 'smtp.gmail.com' #Email Server (don't change!)
@@ -45,9 +45,11 @@ if __name__ == "__main__":
 			print(e)
 		else:
 			current_time = datetime.now()
+			torrent_list = re.sub(r"\\n", '<br>', str(torrent_list.communicate()[0]))
+			torrent_list = re.sub(r"b\'", '', str(torrent_list))
 			emailContent = 'Torrent \'' + str(passedParameter1) + '\' completed. Log in to verify.' + \
-				"\r\n\r\nTransmission-CLI Ubuntu - Live Report\r\nUpdated [" + str(current_time.strftime("%Y-%m-%d %H:%H:%S")) +\
-				"]\r\n--------------------------\r\n" + str(torrent_list.communicate()[0])
+				"<br><br><p style=\"font-family: courier; white-space: pre-wrap; font-size: 10px;\"><b>Transmission-CLI Ubuntu - Live Report<br>Updated [" + str(current_time.strftime("%Y-%m-%d %H:%H:%S")) +\
+				"]</b><br>--------------------------<br>" + str(torrent_list)[:-1] + "</p>"
 
 			try:
 				emailer.sendmail(sendTo, emailSubject, emailContent)
