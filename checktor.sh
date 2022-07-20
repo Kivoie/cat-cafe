0a8771952a51b49f39de107f0763ad3cb70091f4
kivoie <mindarc14@gmail.com>
2022-07-20 19:06:53 -0400

    added sudo

--- checktor.sh
+++ checktor.sh
@@ -23,7 +23,7 @@
 # torrent list. Removes the torrent metadata, not
 # the actual downloaded data.
 
-readonly email='python3 /home/ubuntu/Documents/cat-cafe/sendemail.py'
+readonly email='sudo python3 /home/ubuntu/Documents/cat-cafe/sendemail.py'
 readonly space=' '
 
 while :
@@ -32,16 +32,14 @@ do
 	current_line=$(transmission-remote -l | grep 'Finished  ' | head -1)
 
 	if [ -n "$current_line" ]
-	then
+	then	
 
 		current_torrent_name=$(sed -e 's/.*Finished   //' <<< $current_line)
 		current_torrent_name=${current_torrent_name#$space*$space}
 
 		current_torrent_id=$(awk '{print $1}' <<< $current_line)
 
-		#echo -e "finished line: ${current_line}\ntorrent name: ${current_torrent_name}\ntorrent id: ${current_torrent_id}"
-		
-		$email "$current_torrent_name" && transmission-remote -t ${current_torrent_id} -r
+		$email "${current_torrent_name}" && sudo transmission-remote -t ${current_torrent_id} -r
 
 	fi
 	sleep 30
